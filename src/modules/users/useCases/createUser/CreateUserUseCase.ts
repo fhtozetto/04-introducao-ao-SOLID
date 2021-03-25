9a8bf6b89e777d25bebf89e21ba4357148cb2dce
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-03-25 01:27:38 -0300

    first commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const userAlreadyExistis = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExistis) {
+      throw new Error("User already existis");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
